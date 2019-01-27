febf3ba337752f4b0881312e1428181702e12f44
caleb <griffith.caleb@gmail.com>
2019-01-27 12:24:01 -0500

    added another spider also finished all 7 restful routes

--- project_2/controllers/app_controller.js
+++ project_2/controllers/app_controller.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/users.js');
 const Event = require('../models/events.js')
 const data = require('../models/eventbritedata.js')
+
 //logout button
 router.delete('/', (req, res) => {
     req.session.destroy(()=>{
@@ -22,34 +23,85 @@ router.get('/new',(req,res) => {
 
   router.get('/', (req, res)=>{
       if(req.session.currentUser){
-          res.render('./app/pghindex.ejs');
-      } else {
-          res.redirect('/sessions/new');
-      }
-});
+        Event.
+  find({
+  }).
+  sort({ dateAndTime: -1 }).
+  exec((err,foundEvents) => {
+     res.render('./app/pghindex.ejs',{
+       events:foundEvents
+     });
+  });
+} else{res.redirect('/sessions/new')}
+})
+
 //create
 
 router.post('/new',(req,res) => {
+if(req.session.currentUser){
   Event.create(req.body,(err,createdEvent) => {
-    console.log(req.body);
-    res.send(createdEvent)
+    console.log(createdEvent);
+    res.redirect('/pghfree')
   })
+} else{res.redirect('/sessions/new')}
 })
+
+//show
+router.get('/:id', (req, res)=>{
+  if(req.session.currentUser){
+    Event.findById(req.params.id, (err, foundEvent)=>{
+        res.render('./app/pghshow.ejs',{
+          event:foundEvent
+        });
+    });
+  } else {res.redirect('/sessions/new')}
+});
+
+
 
 //seed data from eventbrite
 router.get('/seed',(req,res) => {
+  // login needed
   Event.create(data,(err,data) => {
     console.log(data);
   })
 })
 
 
+//delete
+router.delete('/:id',(req,res) => {
+  if(req.session.currentUser){
+  Event.findByIdAndRemove(req.params.id,(err,data) => {
+      res.redirect('/pghfree')
+  })
+} else {res.redirect('/sessions/new')}
+})
+
+// edit
+
+router.get('/:id/edit', (req, res)=>{
+  if(req.session.currentUser){
+    Event.findById(req.params.id, (err, foundEvent)=>{
+        res.render(
+    		'./app/pghedit.ejs',
+    		{
+    			event: foundEvent
+    		}
+    	);
+    });
+  } else {res.redirect('/sessions/new')}
+});
+
+//put
 
 
-
-
-
-
+router.put('/:id', (req, res)=>{
+  if(req.session.currentUser){
+    Event.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel)=>{
+        res.redirect('/pghfree');
+    });
+  } else {res.redirect('/sessions/new')}
+});
 
 
 
